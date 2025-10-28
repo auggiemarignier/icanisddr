6b8d2fc3f493e5e952f42f9a852525a66a7ef816
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2025-10-28 20:51:16 +0000

    test: rotation function unit tests

--- tests/test_tti/test_rotation.py
+++ tests/test_tti/test_rotation.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 
 from tti.rotation import (
+    bonds_law_einsum,
     rotation_matrix_x,
     rotation_matrix_y,
     rotation_matrix_z,
@@ -121,3 +122,55 @@ def test_rotation_matrix_zy(rng: np.random.Generator) -> None:
     )
 
     np.testing.assert_array_almost_equal(Rzy, expected)
+
+
+def test_bond_tensor_voigt(rng: np.random.Generator) -> None:
+    """Test that the bond tensor in Voigt notation is symmetric."""
+
+    r = rotation_matrix_z(rng.uniform(0, 2 * np.pi))
+    R = bonds_law_einsum(r)
+
+    # get the notation the same as in Brett et al., 2024
+    r11 = r[0, 0]
+    r12 = r[0, 1]
+    r13 = r[0, 2]
+    r21 = r[1, 0]
+    r22 = r[1, 1]
+    r23 = r[1, 2]
+    r31 = r[2, 0]
+    r32 = r[2, 1]
+    r33 = r[2, 2]
+
+    expected = np.array(
+        [
+            [r11**2, r12**2, r13**2, 2 * r12 * r13, 2 * r11 * r13, 2 * r11 * r12],
+            [r21**2, r22**2, r23**2, 2 * r22 * r23, 2 * r21 * r23, 2 * r21 * r22],
+            [r31**2, r32**2, r33**2, 2 * r32 * r33, 2 * r31 * r33, 2 * r31 * r32],
+            [
+                r21 * r31,
+                r22 * r32,
+                r23 * r33,
+                r22 * r33 + r23 * r32,
+                r21 * r33 + r23 * r31,
+                r21 * r32 + r22 * r31,
+            ],
+            [
+                r11 * r31,
+                r12 * r32,
+                r13 * r33,
+                r12 * r33 + r13 * r32,
+                r11 * r33 + r13 * r31,
+                r11 * r32 + r12 * r31,
+            ],
+            [
+                r11 * r21,
+                r12 * r22,
+                r13 * r23,
+                r12 * r23 + r13 * r22,
+                r11 * r23 + r13 * r21,
+                r11 * r22 + r12 * r21,
+            ],
+        ]
+    )
+
+    np.testing.assert_array_almost_equal(R, expected)
