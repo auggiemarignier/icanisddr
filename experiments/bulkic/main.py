2ac8fe26ea59760a3e4072340fa828ccac9d2cab
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2025-11-13 12:14:06 +0000

    feat: create a separate `bulkic` project that can be run with `uv run bulkic`

---
+++ experiments/bulkic/main.py
@@ -0,0 +1,11 @@
+"""Main entry point for the bulkic experiment."""
+
+
+def main():
+    """Run the bulkic experiment."""
+
+    print("Hello from bulkic!")
+
+
+if __name__ == "__main__":
+    main()
