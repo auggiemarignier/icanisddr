ce10c9ebf89b02391d17b6e2875ccb738072cb68
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-01-13 15:21:47 +0000

    feat: check fully isotropic hypothesis

--- experiments/bulkic/run_sddr.py
+++ experiments/bulkic/run_sddr.py
@@ -2,14 +2,15 @@
 
 import logging
 import pickle
+from functools import partial
 from pathlib import Path
 from typing import Any
 
 import numpy as np
-from bulkic.config import load_config
+from bulkic.config import Config, load_config
 from harmonic.model import RealNVPModel
 from harmonic.sddr import sddr as hmsddr
-from sampling.priors import CompoundPrior
+from sampling.priors import CompoundPrior, PriorFunction
 from sddr.marginalisation import marginalise_prior, marginalise_samples
 from sddr.sddr import (
     RealNVPConfig,
@@ -49,6 +50,52 @@ def configure_posterior_fit(
     )
 
 
+def run_sddr_experiment(
+    samples: np.ndarray,
+    indices: list[int],
+    prior: PriorFunction,
+    nu: np.ndarray,
+    cfg: Config,
+) -> None:
+    """Run the synthetic bulk IC SDDR experiment."""
+
+    logger.info("Configuring posterior fitting parameters")
+    train_cfg, realnvp_cfg = configure_posterior_fit(cfg.model_dump())
+    logger.info("Training flow model on marginalised samples")
+    marg_posterior = fit_marginalised_posterior(
+        samples, indices, realnvp_cfg, train_cfg
+    )
+
+    logger.info("Marginalising the prior")
+    marg_prior = marginalise_prior(prior, indices)
+
+    logger.info("Calculating Savage-Dickey density ratio at point nu=0")
+    sddr_h1 = sddr(marg_posterior, marg_prior, nu)
+    logger.info(f"logSDDR for hypothesis 1 (vertical symmetry axis): {sddr_h1:.4f}")
+
+    if sddr_h1 > 0:
+        logger.info("Evidence in favour of nested model")
+    else:
+        logger.info("Evidence against nested model")
+
+    logger.info("Experiment complete")
+
+    logger.info("Calculating harmonic SDDR for comparison")
+    flow_model = RealNVPModel(ndim_in=marg_prior.n, standardize=True, temperature=1.0)
+    harm_sddr = hmsddr(flow_model, marginalise_samples(samples, indices))
+    flow_log_bf, flow_log_bf_std = harm_sddr.log_bayes_factor(
+        log_prior=marg_prior(nu),
+        value=nu,
+        nbootstraps=10,
+        bootstrap_proportion=0.5,
+        bootstrap=True,
+        epochs=10,
+    )
+    logger.info(
+        f"Harmonic logSDDR for hypothesis 1 (vertical symmetry axis): {flow_log_bf:.4f} ± {flow_log_bf_std:.4f}"
+    )
+
+
 CFG_FILE = Path(__file__).parent / "config.yaml"
 SAMPLES_PATH = Path(__file__).parent / "samples.pkl"
 
@@ -64,45 +111,21 @@ def main() -> None:
     logger.info("Setting up prior")
     prior = CompoundPrior.from_dict(cfg.priors.model_dump())
 
+    run_experiment = partial(run_sddr_experiment, samples=samples, prior=prior, cfg=cfg)
+
     # Hypothesis 1: Vertical symmetry axis (eta1 = 0, eta2 = 0)
-    # => margninalise out the love parameters, keeping the last two
+    # => marginalise out the love parameters, keeping the last two
     indices = [3, 4]  # Indices of eta1 and eta2
+    nu = np.array([0.0, 0.0])
+    logger.info("Running experiment for hypothesis 1 (vertical symmetry axis)")
+    run_experiment(indices=indices, nu=nu)
 
-    logger.info("Configuring posterior fitting parameters")
-    train_cfg, realnvp_cfg = configure_posterior_fit(cfg.model_dump())
-    logger.info("Training flow model on marginalised samples")
-    marg_posterior = fit_marginalised_posterior(
-        samples, indices, realnvp_cfg, train_cfg
-    )
-
-    logger.info("Marginalising the prior")
-    marg_prior = marginalise_prior(prior, indices)
-
-    nu = np.zeros(2)  # evaluation point
-    logger.info("Calculating Savage-Dickey density ratio at point nu=0")
-    sddr_h1 = sddr(marg_posterior, marg_prior, nu)
-    logger.info(f"logSDDR for hypothesis 1 (vertical symmetry axis): {sddr_h1:.4f}")
-
-    if sddr_h1 > 0:
-        logger.info("Evidence in favour of nested model")
-    else:
-        logger.info("Evidence against nested model")
-
-    logger.info("Experiment complete")
-
-    flow_model = RealNVPModel(ndim_in=marg_prior.n, standardize=True, temperature=1.0)
-    harm_sddr = hmsddr(flow_model, marginalise_samples(samples, indices))
-    flow_log_bf, flow_log_bf_std = harm_sddr.log_bayes_factor(
-        log_prior=marg_prior(nu),
-        value=nu,
-        nbootstraps=10,
-        bootstrap_proportion=0.5,
-        bootstrap=True,
-        epochs=10,
-    )
-    logger.info(
-        f"Harmonic logSDDR for hypothesis 1 (vertical symmetry axis): {flow_log_bf:.4f} ± {flow_log_bf_std:.4f}"
-    )
+    # Hypothesis 2: Fully Isotropic (A=C=F, L=N=0, eta1=0, eta2=0)
+    # => marginalise out A, evaluate everywhere else at 0
+    indices = list(range(1, prior.n))
+    nu = np.array([0.0, 0.0, 0.0, 0.0])  # C-A, F-A+2N, eta1, eta2
+    logger.info("Running experiment for hypothesis 2 (fully isotropic)")
+    run_experiment(indices=indices, nu=nu)
 
 
 if __name__ == "__main__":
