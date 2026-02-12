f4342496c5728af51efdd78e95d50b3c0663dec4
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-02-12 13:04:32 +0000

    feat: average rho vp vs

--- src/icprem/src/icprem/__init__.py
+++ src/icprem/src/icprem/__init__.py
@@ -1,5 +1,10 @@
 """Entry point for the icprem package."""
 
 from .love import prem as PREM
+from .model import average
 
-__all__ = ["PREM"]
+PREM_IC_RHO = average("density", radius=1221.5)
+PREM_IC_VP = average("vp", radius=1221.5)
+PREM_IC_VS = average("vs", radius=1221.5)
+
+__all__ = ["PREM", "PREM_IC_RHO", "PREM_IC_VP", "PREM_IC_VS"]
