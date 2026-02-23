dcb38226c5a164cd2c1feb33a7e2bfcea64648ef
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-02-23 10:14:16 +0100

    feat: gradients wrt eta

--- src/tti/src/tti/elastic/voigt.py
+++ src/tti/src/tti/elastic/voigt.py
@@ -1,15 +1,12 @@
 """Elastic tensors as Voigt notation matrices."""
 
-from tti.rotation import rotation_matrix_z, rotation_matrix_y
-
 import numpy as np
 
 from ..rotation import (
-    gradient_rotation_matrix_y,
-    gradient_rotation_matrix_z,
+    gradient_rotation_matrix_zy,
     rotation_matrix_zy,
 )
-from .voigt_mapping import matrix_to_voigt
+from .voigt_mapping import gradient_matrix_to_voigt, matrix_to_voigt
 
 
 def isotropic_tensor(lam: np.ndarray, mu: np.ndarray) -> np.ndarray:
@@ -462,3 +459,93 @@ def gradient_D_wrt_N(
     """
     Rzy = matrix_to_voigt(rotation_matrix_zy(eta1, eta2))
     return Rzy @ dCdN @ Rzy.T
+
+
+def gradient_D_wrt_eta1(
+    A: np.ndarray,
+    C: np.ndarray,
+    F: np.ndarray,
+    L: np.ndarray,
+    N: np.ndarray,
+    eta1: np.ndarray,
+    eta2: np.ndarray,
+) -> np.ndarray:
+    """Gradient of D wrt eta1 evaluated for elastic tensor C at eta1, eta2.
+
+    Parameters
+    ----------
+    A : np.ndarray (...,)
+        Elastic constant C11 = C22.
+    C : np.ndarray (...,)
+        Elastic constant C33.
+    F : np.ndarray (...,)
+        Elastic constant C13 = C23.
+    L : np.ndarray (...,)
+        Elastic constant C44 = C55.
+    N : np.ndarray (...,)
+        Elastic constant C66.
+    eta1 : np.ndarray (...,)
+        Tilt angle around the y-axis (in radians)
+    eta2 : np.ndarray (...,)
+        Azimuthal angle around the z-axis (in radians)
+
+    Returns
+    -------
+    dDdeta1 : np.ndarray (..., 6, 6)
+    """
+    elastic_tensor = transverse_isotropic_tensor(A, C, F, L, N)
+
+    dReta1, _ = gradient_rotation_matrix_zy(eta1, eta2)
+    R = rotation_matrix_zy(eta1, eta2)
+    dReta1_voigt = gradient_matrix_to_voigt(R, dReta1)
+
+    R_voigt = matrix_to_voigt(R)
+
+    return dReta1_voigt @ elastic_tensor @ R_voigt.swapaxes(-1, -2) + R_voigt @ (
+        elastic_tensor @ dReta1_voigt.swapaxes(-1, -2)
+    )
+
+
+def gradient_D_wrt_eta2(
+    A: np.ndarray,
+    C: np.ndarray,
+    F: np.ndarray,
+    L: np.ndarray,
+    N: np.ndarray,
+    eta1: np.ndarray,
+    eta2: np.ndarray,
+) -> np.ndarray:
+    """Gradient of D wrt eta2 evaluated for elastic tensor C at eta1, eta2.
+
+    Parameters
+    ----------
+    A : np.ndarray (...,)
+        Elastic constant C11 = C22.
+    C : np.ndarray (...,)
+        Elastic constant C33.
+    F : np.ndarray (...,)
+        Elastic constant C13 = C23.
+    L : np.ndarray (...,)
+        Elastic constant C44 = C55.
+    N : np.ndarray (...,)
+        Elastic constant C66.
+    eta1 : np.ndarray (...,)
+        Tilt angle around the y-axis (in radians)
+    eta2 : np.ndarray (...,)
+        Azimuthal angle around the z-axis (in radians)
+
+    Returns
+    -------
+    dDdeta2 : np.ndarray (..., 6, 6)
+    """
+    elastic_tensor = transverse_isotropic_tensor(A, C, F, L, N)
+
+    _, dReta2 = gradient_rotation_matrix_zy(eta1, eta2)
+    R = rotation_matrix_zy(eta1, eta2)
+    dReta2_voigt = gradient_matrix_to_voigt(R, dReta2)
+
+    R_voigt = matrix_to_voigt(R)
+
+    return dReta2_voigt @ elastic_tensor @ R_voigt.swapaxes(-1, -2) + R_voigt @ (
+        elastic_tensor @ dReta2_voigt.swapaxes(-1, -2)
+    )
--- src/tti/src/tti/elastic/voigt_mapping.py
+++ src/tti/src/tti/elastic/voigt_mapping.py
@@ -202,3 +202,110 @@ def matrix_to_voigt(R: np.ndarray) -> np.ndarray:
         axis=-1,
     )
     return np.stack([row0, row1, row2, row3, row4, row5], axis=-2)
+
+
+def gradient_matrix_to_voigt(R: np.ndarray, dR: np.ndarray) -> np.ndarray:
+    """Compute derivative of the Bond Voigt transform `matrix_to_voigt(R)` given `R` and its derivative `dR`.
+
+    This applies the product / chain rule elementwise to the mapping in `matrix_to_voigt` so callers can obtain dR_voigt/dtheta = d(matrix_to_voigt(R))/dtheta.
+
+    Parameters
+    ----------
+    R : ndarray, shape (..., 3, 3)
+        Rotation matrix.
+    dR : ndarray, shape (..., 3, 3)
+        Derivative of the rotation matrix with respect to a scalar.
+
+    Returns
+    -------
+    dR_voigt : ndarray, shape (..., 6, 6)
+        Derivative of the 6x6 Voigt transformation matrix.
+    """
+    r11 = R[..., 0, 0]
+    r12 = R[..., 0, 1]
+    r13 = R[..., 0, 2]
+    r21 = R[..., 1, 0]
+    r22 = R[..., 1, 1]
+    r23 = R[..., 1, 2]
+    r31 = R[..., 2, 0]
+    r32 = R[..., 2, 1]
+    r33 = R[..., 2, 2]
+
+    dr11 = dR[..., 0, 0]
+    dr12 = dR[..., 0, 1]
+    dr13 = dR[..., 0, 2]
+    dr21 = dR[..., 1, 0]
+    dr22 = dR[..., 1, 1]
+    dr23 = dR[..., 1, 2]
+    dr31 = dR[..., 2, 0]
+    dr32 = dR[..., 2, 1]
+    dr33 = dR[..., 2, 2]
+
+    row0 = np.stack(
+        [
+            2 * r11 * dr11,
+            2 * r12 * dr12,
+            2 * r13 * dr13,
+            2 * (dr12 * r13 + r12 * dr13),
+            2 * (dr11 * r13 + r11 * dr13),
+            2 * (dr11 * r12 + r11 * dr12),
+        ],
+        axis=-1,
+    )
+    row1 = np.stack(
+        [
+            2 * r21 * dr21,
+            2 * r22 * dr22,
+            2 * r23 * dr23,
+            2 * (dr22 * r23 + r22 * dr23),
+            2 * (dr21 * r23 + r21 * dr23),
+            2 * (dr21 * r22 + r21 * dr22),
+        ],
+        axis=-1,
+    )
+    row2 = np.stack(
+        [
+            2 * r31 * dr31,
+            2 * r32 * dr32,
+            2 * r33 * dr33,
+            2 * (dr32 * r33 + r32 * dr33),
+            2 * (dr31 * r33 + r31 * dr33),
+            2 * (dr31 * r32 + r31 * dr32),
+        ],
+        axis=-1,
+    )
+    row3 = np.stack(
+        [
+            dr21 * r31 + r21 * dr31,
+            dr22 * r32 + r22 * dr32,
+            dr23 * r33 + r23 * dr33,
+            dr22 * r33 + r22 * dr33 + dr23 * r32 + r23 * dr32,
+            dr21 * r33 + r21 * dr33 + dr23 * r31 + r23 * dr31,
+            dr21 * r32 + r21 * dr32 + dr22 * r31 + r22 * dr31,
+        ],
+        axis=-1,
+    )
+    row4 = np.stack(
+        [
+            dr11 * r31 + r11 * dr31,
+            dr12 * r32 + r12 * dr32,
+            dr13 * r33 + r13 * dr33,
+            dr12 * r33 + r12 * dr33 + dr13 * r32 + r13 * dr32,
+            dr11 * r33 + r11 * dr33 + dr13 * r31 + r13 * dr31,
+            dr11 * r32 + r11 * dr32 + dr12 * r31 + r12 * dr31,
+        ],
+        axis=-1,
+    )
+    row5 = np.stack(
+        [
+            dr11 * r21 + r11 * dr21,
+            dr12 * r22 + r12 * dr22,
+            dr13 * r23 + r13 * dr23,
+            dr12 * r23 + r12 * dr23 + dr13 * r22 + r13 * dr22,
+            dr11 * r23 + r11 * dr23 + dr13 * r21 + r13 * dr21,
+            dr11 * r22 + r11 * dr22 + dr12 * r21 + r12 * dr21,
+        ],
+        axis=-1,
+    )
+
+    return np.stack([row0, row1, row2, row3, row4, row5], axis=-2)
